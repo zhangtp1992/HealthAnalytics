6b21159b827070cedf7f343e603ee34ddffdcb82
itomaldonado <mo.maldonado@gmail.com>
2015-11-28 16:17:30 -0500

    Updated SAS export script to put things on a file.

--- 4_data_collection/CDC/export_sas_data.py
+++ 4_data_collection/CDC/export_sas_data.py
@@ -1,5 +1,8 @@
-import xport, csv, json
+import xport, csv, json, os
 from datetime import datetime
+
+# Output Directory
+output = './outputs/'
 
 # List of SAS files to export
 files_SAS = [
@@ -60,23 +63,29 @@ def json_serial(obj):
     return serial
   raise TypeError ("Type not serializable")
 
+
+# Crete the output folder if needed...
+if not os.path.exists(output):
+  os.makedirs(output)
+
+# Go through each SAS file and do work
 for file_name in files_SAS:
   # Open SAS data file
   with xport.XportReader(file_name) as reader:
     # Writing the data legend.
-    with open(file_name + '_legend.json', 'wb') as legend:
+    with open(output+ file_name + '_legend.json', 'wb') as legend:
       legend_txt = json.dumps(reader.fields, sort_keys=True, indent=2, separators=(',', ': '), default=json_serial)
       legend.write(legend_txt)
     # Writing data file info.
-    with open(file_name + '_file_info.json', 'wb') as info:
+    with open(output+ file_name + '_file_info.json', 'wb') as info:
       info_txt = json.dumps(reader.file_info, sort_keys=True, indent=2, separators=(',', ': '), default=json_serial)
       info.write(info_txt)
     # Writing data file info.
-    with open(file_name + '_member_info.json', 'wb') as info:
+    with open(output+ file_name + '_member_info.json', 'wb') as info:
       info_txt = json.dumps(reader.member_info, sort_keys=True, indent=2, separators=(',', ': '), default=json_serial)
       info.write(info_txt)
     # Writing data to CSV
-    with open(file_name + '.csv', 'wb') as out: # add .csv at the end
+    with open(output+ file_name + '.csv', 'wb') as out: # add .csv at the end
       # first, open dict writer and add the header labels
       writer = csv.DictWriter(out, fieldnames=[f['name'] for f in reader.fields])
       writer.writeheader()
